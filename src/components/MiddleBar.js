ac2b35bab90520b827200cae4988d883a09db56b
MikkeBee <mjblakley87@gmail.com>
2022-05-09 23:23:49 +0300

    added lower nav boxes, placeholder text, and styling

--- src/components/MiddleBar.js
+++ src/components/MiddleBar.js
@@ -2,15 +2,30 @@ import React from "react";
 
 const MiddleBar = () => {
   return (
-    <div className="middleBarDiv">
-      <div className="carouselDiv">
-        <p>Placeholder</p>
-      </div>
-      <div className="carouselDiv">
-        <p>Placeholder</p>
-      </div>
-      <div className="carouselDiv">
-        <p>Placeholder</p>
+    <div>
+      <h3>Looking for something?</h3>
+      <div className="middleBarDiv">
+        <div className="carouselDiv">
+          <h4>Placeholder</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="carouselDiv">
+          <h4>Placeholder</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="carouselDiv">
+          <h4>Placeholder</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
       </div>
     </div>
   );

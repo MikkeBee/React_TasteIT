32eb76ac8fc0548d54bac9761f2ac57ede126a3b
MikkeBee <mjblakley87@gmail.com>
2022-05-09 14:04:58 +0300

    add more components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./index.css";
+import Recipes from "./components/Recipes";
+import AddaRecipe from "./components/AddaRecipe";
 import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
+import Layout from "./pages/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Layout />} />
+          <Route path="recipes" element={<Recipes />} />
+          <Route path="addarecipe" element={<AddaRecipe />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/recipes">Recipes</Link>
+        </li>
+        <li>
+          <Link to="/addarecipe">Add a Recipe</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;

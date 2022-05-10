e6a7d33947aefc82a1b30f6bbffe4fdffafba2db
MikkeBee <mjblakley87@gmail.com>
2022-05-10 14:34:08 +0300

    updated card styling and getting the main data formatted

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import "./index.css";
 import Recipes from "./components/Recipes";
 import AddaRecipe from "./components/AddaRecipe";
 import Home from "./components/Home";
-import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import axios from "axios";
 import Layout from "./pages/Layout";
 
 function App() {
